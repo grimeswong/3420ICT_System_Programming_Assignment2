eef0d99ce7d6b072c738a7045b8e520f2deda17c
Shadow Grimes <grimeswong@gmail.com>
2017-04-16 16:19:48 +1000

    finished read line loop and trim newline char

--- milestone1/sync.c
+++ milestone1/sync.c
@@ -6,12 +6,45 @@
 
 /*** Preprocessor ***/
 #include <stdio.h>    //Standard Input/Output library eg. getchar, gets, putchar, puts, sprintf
+#include <stdlib.h>   //General Utilities library eg. atoi, atof, malloc, realloc
+#include <string.h>   //String handling library eg. strcpy, strcat, strcmp
 
+#include <pthread.h>  //pthread_create, pthread_join, pthread_exit
+
+#define BUFFERSIZE 128
+
+/*** function declartion ***/
+void trimnewline(char *string);
 
 int main() {
+  char charMix[128];
+  char *charPtr = &charMix[0];            // assign pointer point to the first element of the charMix
 
-  printf("Hello world!!!\n");
+  fgets(charPtr, BUFFERSIZE, stdin);
+  while(charPtr != NULL) {
+    trimnewline(charPtr);
+    
 
+    printf("%s\n", charPtr);
+
+
+
+    fgets(charPtr, BUFFERSIZE, stdin);
+  }
 
   return 0;
+}
+
+
+
+/*  function: trim the '\n' of the line of a string
+ *  parameter: the pointer address of string
+ *  return: nil
+ */
+void trimnewline(char *string) {
+  int pos = strlen(string) - 1;                                 //second last of the string array
+
+  if(string != NULL) {
+    string[pos] = '\0';
+  }
 }
