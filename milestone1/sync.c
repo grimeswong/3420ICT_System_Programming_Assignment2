03687f8c279e6bdc8f774db41bfae8d405766446
Shadow Grimes <grimeswong@gmail.com>
2017-04-16 14:30:49 +1000

    added makefile (target: clean and all) and sync.c

---
+++ milestone1/sync.c
@@ -0,0 +1,17 @@
+//  3420ICT System Programming
+//  SID: S2193948
+//  Author: Chun Hung Wong (Grimes)
+//  Program: sync.c
+//  Purpose: Use to create a thread for synchronisation
+
+/*** Preprocessor ***/
+#include <stdio.h>    //Standard Input/Output library eg. getchar, gets, putchar, puts, sprintf
+
+
+int main() {
+
+  printf("Hello world!!!\n");
+
+
+  return 0;
+}
