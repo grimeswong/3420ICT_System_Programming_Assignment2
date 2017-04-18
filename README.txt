3232ab83f903d9824687b286eb1d56d71e4bb8ce
Shadow Grimes <grimeswong@gmail.com>
2017-04-18 14:34:25 +1000

    Added a mutex and structrue to pass the status of mutex that the child thread wait for the parent to read, then print the line

---
+++ README.txt
@@ -0,0 +1,14 @@
+Q3 .)In the parent thread, read one line of input into the buffer.
+Print the buffer to stdout from within the child thread (do not synchronise with the parent at this stage).
+Explain in your README what happens and why!
+
+A3. After create the child thread, the parent read a line of code and store it in the specified memory address(array),
+and a pointer pointer to the beginning of the address, and the child thread print the line of code by the address of pointer
+However, both thread happen at the same time (racing), if the child thread print the line first (read the buffer first and the ),
+then the result is nothing or just part of the strings on the screen.
+So the current situation is, the child thread print nothing on the screen because child thread is faster than the main thread
+something strange code output (h?), h?>
+
+Q5. After the child thread has printed the output, make the parent thread prompt the user to hit Enter
+(and wait for the user to do so). Hint: You will need a second mutex for this. 
+Explain in your README why this cannot be done reliably with a single mutex!
--- milestone1/sync.c
+++ milestone1/sync.c
@@ -8,34 +8,101 @@
 #include <stdio.h>    //Standard Input/Output library eg. getchar, gets, putchar, puts, sprintf
 #include <stdlib.h>   //General Utilities library eg. atoi, atof, malloc, realloc
 #include <string.h>   //String handling library eg. strcpy, strcat, strcmp
-
-#include <pthread.h>  //pthread_create, pthread_join, pthread_exit
+#include <unistd.h>   //eg. sleep
+#include <pthread.h>  //eg. pthread_create, pthread_join, pthread_exit
 
 #define BUFFERSIZE 128
 
 /*** function declartion ***/
 void trimnewline(char *string);
+void *printline(void *string);
+
+/*** Mutex intialization ***/
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/*** Structure to store state of mutex the line of string ***/
+struct dataMutex {
+  int mutexState; // 0 for unlock, 1 for lock
+  char *linePtr;
+
+}; // must put semicolon after this structure
+
+
+
 
 int main() {
-  char charMix[128];
-  char *charPtr = &charMix[0];            // assign pointer point to the first element of the charMix
+  /*** Local variables ***/
+  char charMix[BUFFERSIZE];               // Create an array for maximum 128 characters
+  // char *charPtr = &charMix[0];            // Assign pointer point to the first element of the charMix
+  int rt;                                 // return 0 = successful, -1 = fail
 
-  fgets(charPtr, BUFFERSIZE, stdin);
-  while(charPtr != NULL) {
-    trimnewline(charPtr);
-    
-
-    printf("%s\n", charPtr);
+  struct dataMutex dataMutex1, *dataMutexPtr;            // Declare of the dataMutex
+  dataMutex1.mutexState = 0;              // 0 for unlock, 1 for lock
+  dataMutex1.linePtr = &charMix[0];       // Assign the pointer point to the first element of the charMix
+  dataMutexPtr = &dataMutex1;
 
 
+  // Thread ID:
+  pthread_t tid;                     // pthread data structure
 
-    fgets(charPtr, BUFFERSIZE, stdin);
+  // Create pthread attributes
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+
+
+  /*** to do list ***/
+  // create thread first than read the line
+
+  /*** Create the child thread ***/
+  rt = pthread_create(&tid, &attr, printline, (void *) dataMutexPtr);    //create thread
+  if(rt != 0) {
+    perror("Couldn't create a thread!!!");
   }
+
+  /*** read and print line in the parent thread ***/
+  pthread_mutex_lock(&mutex);
+  // dataMutex1.mutexState = 1;  // 1 = locked
+  fgets(dataMutex1.linePtr, BUFFERSIZE, stdin);      // Read the line
+  sleep(2); // debugger:
+  printf("Parent thread: dataMutex1linePtr is %s\n", dataMutexPtr->linePtr);      // debugger:
+  printf("Parent thread: dataMutex1.mutexState is %d\n", (int)dataMutexPtr->mutexState);      // debugger:
+  pthread_mutex_unlock(&mutex);
+  dataMutex1.mutexState = 0; // 0 = unlocked
+
+  // rt = pthread_create(&tid, &attr, printline, (void *) dataMutexPtr);    //create thread
+
+  pthread_join(tid, NULL);    //  wait to other thread to join
+
+  /*** end: read and print line thread ***/
+
+  // Destroy everthing(mutex, cond & attr) and confirm all threads are exit
+  // pthread_mutex_destroy()
+  pthread_exit(NULL);       // The last thing the main should do
 
   return 0;
 }
 
 
+/*  function: print out the line of user entered
+*  parameter: the pointer address of string
+*  return: nil
+*/
+
+void *printline(void *dataMutexPtr) {
+
+  sleep(2);  //debugger
+  struct dataMutex *mydataMutex = (struct dataMutex*) dataMutexPtr; //type casting:
+  if((*mydataMutex->linePtr) == 1) {
+    // do nothing
+    printf("Child thread: parent thread is locked\n");
+  } else {
+    pthread_mutex_lock(&mutex);
+    printf("Child thread: printline: %s\n", (*mydataMutex).linePtr);    // debugger:
+    printf("%s", (*mydataMutex).linePtr);
+  }
+  return NULL;  // to silence the non-void function
+  // or pthread_exit Child thread call to exit
+}
 
 /*  function: trim the '\n' of the line of a string
  *  parameter: the pointer address of string
@@ -43,7 +110,6 @@ int main() {
  */
 void trimnewline(char *string) {
   int pos = strlen(string) - 1;                                 //second last of the string array
-
   if(string != NULL) {
     string[pos] = '\0';
   }
